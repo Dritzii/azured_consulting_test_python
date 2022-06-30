4f8432001bdbcc953a02e9549e6366ad6746f75c
John Pham <john.manual@outlook.com>
2022-06-30 18:55:15 +1000

    added more questions

---
+++ azured_pythontest.py
@@ -0,0 +1,191 @@
+"""
+Author: John Pham
+Company: Azured Consulting
+Meta: This test should take 30 minutes - 1 hour of your time.
+"""
+# ADD your imports here.
+
+
+
+
+# https://catfact.ninja/                    <---- SWAGGER with API Documentation.
+
+# 1. Write a API call towards Cat Fact API and return the first 10 results from the /facts endpoint.
+# You can use the relative swagger website to get an example of the data returned.
+# you can use any http library you like.
+
+
+
+
+
+
+
+# 2. For each of the item objects returned from the API above, add a count to each json object in the list.
+# please see example of the 2 dicts below.
+example = {
+      "fact": "Unlike dogs, cats do not have a sweet tooth. Scientists believe this is due to a mutation in a key taste receptor.",
+      "length": 114
+    }
+
+adding_index_to_example = {
+      "count" : 1 ,
+      "fact": "Unlike dogs, cats do not have a sweet tooth. Scientists believe this is due to a mutation in a key taste receptor.",
+      "length": 114
+    }
+
+
+
+
+
+
+
+
+
+# 3. Use Pandas to turn the list of objects into a Table.
+# https://pandas.pydata.org/docs/index.html for more documentation.
+
+
+
+
+
+
+
+
+# 4. For the Table for the above question 3, write a sql query to grab the top 1 results containing the number 24 using Pandas.
+
+
+
+
+
+
+
+# 5. In Azure there is an equilivent of an S3 bucket in Amazon, what is this?.
+
+
+
+
+
+
+# 6. How would you install a new Package/s for Python in the command line in a requirements.txt file?.
+
+
+
+
+
+
+# 7. What is a pull request?.
+
+
+
+
+
+
+# 8. What is a way to hide your environment variables in Python? - Will also accept a C#/.Net version of this.
+
+
+
+
+
+
+# 9. Explain recursion and write an example method of a recursion.
+
+
+
+
+
+
+# 10. Create a SQL Function also known as T-SQL to get the bottom last 50 rows of an imaginary table everytime you run it.
+
+
+
+
+
+
+
+# 11. How would you check to see if a list contains a key, make an example below with the Try and Except keyword.
+
+
+
+
+
+
+
+
+# 12. How would you check to see if a Dictionary contains a key, make an example below with the Try and Except Keyword.
+
+
+
+
+
+
+# 13. What is a class in python? Make an example below with School being the class object
+# with 1 method returning a first name as string and 1 method returning a last name as string.
+
+
+
+
+
+# 14. What is a Static Method in Python? use the above class to create an example.
+
+
+
+
+
+
+# 15. What is a constructor in OOP?.
+
+
+
+
+
+
+
+# 16. In the class created above, include a constructor with 2 class variables that are defined on object creation.
+
+
+
+
+
+
+# 17. how would you access a Static Method?
+
+
+
+
+
+
+# 18. Create a new Class named Student that will inherit from the class student created above
+
+
+
+
+
+
+
+
+
+# 19. In your Student class, add 2 extra class variables towards the constructor, they will be school name and address
+
+
+
+
+
+
+
+
+# 20. After you have created the 2 classes above, create 2 class objects 1 from school and 1 from student.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
